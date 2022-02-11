6bc5493969cd8069d7a7e4de8cff323d8bb90088
SaraMisajlovska <smisajlovska@gmail.com>
2022-02-11 16:44:51 +0100

    module 3
    -create react app
    -jsx
    -components
    -state and handler events
    - binding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      {
+        name: 'Max',
+        age: 28
+      },
+      {
+        name: 'Manu',
+        age: 29
+      }, {
+        name: 'Stephanie',
+        age: 25
+      }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log('was clicked')
+    // this is wrong this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        {
+          name: newName,
+          age: 28
+        },
+        {
+          name: 'Manu',
+          age: 29
+        }, {
+          name: 'Stephanie',
+          age: 27
+        }
+      ]
+    })
+
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {
+          name: 'Max',
+          age: 28
+        },
+        {
+          name: event.target.value,
+          age: 29
+        }, {
+          name: 'Stephanie',
+          age: 25
+        }
+      ]
+    })
+  }
+
+  render() {
+    const style={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    return (
+      //this is jsx code, not html even though it looks like it 
+      //bind syntax is better than arrow function for switch name 
+      <div className="App">
+        <h1>Hi, I'm Sara Misajlovska.</h1>
+        <button 
+        onClick={() => this.switchNameHandler('Maxus')}
+        style={style}
+        >Switch name</button>
+
+        <Person
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Maximilian')} />
+
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          changed = {this.nameChangedHandler}/>
+
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, 'Max!!')}> 
+           My hobbies: Racing
+        </Person>
+      </div>
+    );
+
+    // return React.createElement( 'div',{className:'App'},React.createElement('h1',null, 'Hi, I\'m Sara Misajlovska!!! This works'))
+  }
 }
 
 export default App;
